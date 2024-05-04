d20c71be1764a387aa0c0a7dddc05cd5a12773d6
peterrbt93 <petertunstall93@gmail.com>
2024-05-03 20:04:03 -0400

    Code added to AccountController to make all unittests pass

--- Microservices/microPay.Accounts/Controllers/AccountsController.cs
+++ Microservices/microPay.Accounts/Controllers/AccountsController.cs
@@ -21,26 +21,134 @@ namespace microPay.Accounts.Controllers
         [HttpGet("GetBalanceByUsername")]
         public async Task<IActionResult> GetBalanceByUsername(string username)
         {
-            throw new NotImplementedException();
+            AccountAmount result = new AccountAmount();
+
+            if (string.IsNullOrEmpty(username))
+            {
+                return new ObjectResult(null) { StatusCode = StatusCodes.Status400BadRequest };
+            }
+
+            try
+            {
+                result = await _accountsService.GetBalanceByUsername(username);
+            }
+            catch (AccountNotExistsException)
+            {
+                result.Username = "NOTEXIST";
+                result.Amount = 0.0;
+                return new ObjectResult(result) { StatusCode = StatusCodes.Status200OK };
+            }
+            catch (Exception ex)
+            {
+                result.Username = ex.Message;
+                result.Amount = 0.0;
+                return new ObjectResult(result) { StatusCode = StatusCodes.Status422UnprocessableEntity };
+            }
+
+            return new ObjectResult(result) { StatusCode = StatusCodes.Status200OK };
         }
 
         [HttpPost("CreateAccount")]
         public async Task<IActionResult> CreateAccount(AccountDTO account)
         {
 
-            throw new NotImplementedException();
+            bool success = false;
+
+            if (string.IsNullOrEmpty(account.Username) 
+                || string.IsNullOrEmpty(account.Password))
+            {
+                return new ObjectResult(success) { StatusCode = StatusCodes.Status400BadRequest };
+            }
+
+            try
+            {
+               success = await _accountsService.CreateAccount(account);
+            }
+            catch (AccountAlreadyExistsException)
+            {
+                return new ObjectResult(success) { StatusCode = StatusCodes.Status409Conflict};
+            }
+            catch (Exception)
+            {
+                return new ObjectResult(success) { StatusCode = StatusCodes.Status422UnprocessableEntity };
+            }
+
+            if (success)
+            {
+                return new ObjectResult(success) { StatusCode = StatusCodes.Status201Created };
+            }
+            else
+            {
+               return new ObjectResult(success) { StatusCode = StatusCodes.Status422UnprocessableEntity };
+            }
         }
 
         [HttpPost("Deposit")]
         public async Task<IActionResult> Deposit(AccountAmount accChangeRequest)
         {
-            throw new NotImplementedException();
+            AccountAmount response = accChangeRequest;
+
+            if (string.IsNullOrEmpty(accChangeRequest.Username) || accChangeRequest.Amount <= 0.0)
+            {
+                response.Username = "Deposit failed - Error in request";
+                return new ObjectResult(response) { StatusCode = StatusCodes.Status400BadRequest };
+            }
+
+            try
+            {
+                response = await _accountsService.DepositOrWithdraw(accChangeRequest, "DEPOSIT");
+            }
+            catch (AccountNotExistsException)
+            {
+                response.Username = "NOTEXIST";
+                response.Amount = 0.0;
+                return new ObjectResult(response) { StatusCode = StatusCodes.Status200OK };
+            }
+            catch (Exception ex)
+            {
+                response.Username = ex.Message;
+                response.Amount = 0.0;
+                return new ObjectResult(response) { StatusCode = StatusCodes.Status422UnprocessableEntity };
+            }
+
+            return new ObjectResult(response) { StatusCode = StatusCodes.Status200OK };
         }
 
         [HttpPost("Withdraw")]
         public async Task<IActionResult> Withdraw(AccountAmount accChangeRequest)
         {
-            throw new NotImplementedException();
+            AccountAmount response = accChangeRequest;
+
+            if (string.IsNullOrEmpty(accChangeRequest.Username) || accChangeRequest.Amount <= 0.0)
+            {
+                response.Username = "Withdraw failed - Error in request";
+                return new ObjectResult(response) { StatusCode = StatusCodes.Status400BadRequest };
+            }
+
+            try
+            {
+                response = await _accountsService.DepositOrWithdraw(accChangeRequest, "WITHDRAW");
+            }
+            catch (AccountNotExistsException)
+            {
+                response.Username = "NOTEXIST";
+                response.Amount = 0.0;
+                return new ObjectResult(response) { StatusCode = StatusCodes.Status200OK };
+            }
+            catch (AccountCannotOverdraftException)
+            {
+                response.Username = "Withdraw failed - Account not allowed overdraft";
+                response.Amount = 0.0;
+                return new ObjectResult(response) { StatusCode = StatusCodes.Status400BadRequest };
+            }
+            catch (Exception ex)
+            {
+                response.Username = ex.Message;
+                response.Amount = 0.0;
+                return new ObjectResult(response) { StatusCode = StatusCodes.Status422UnprocessableEntity };
+            }
+
+            return new ObjectResult(response) { StatusCode = StatusCodes.Status200OK };
         }
     }
 }
